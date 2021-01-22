db1e4fa22c6b66b0ea7d7f5e9e24f8d7dd4e0988
Malte <malte.liam@outlook.com>
2021-01-22 16:51:40 +0100

    Add some basic styling
    Required for future display and organization of users

--- pages/admin/roles.php
+++ pages/admin/roles.php
@@ -14,50 +14,60 @@
   <head>
     <meta charset="utf-8">
     <title>Theatre Planner | Roles</title>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . "/theatre_planner/pages/head.html"; ?>
   </head>
   <body>
     <!-- TODO add sidebar / static nav -->
-    <form action="" method="post">
-      <input required="true" type="text" name="roleName" maxlength="32"><br/>
-      <textarea name="roleDescription" rows="8" cols="64" maxlength="512"></textarea><br/>
-      <input type="submit" name="addRole" value="Create Role">
-    </form>
+    <main class="ui text container">
+      <form action="" method="post" class="ui form">
+        <div class="required field">
+          <label for="roleName">Role Name</label>
+          <input required="true" type="text" name="roleName" maxlength="32">
+        </div>
+        <div class="field">
+          <label for="roleDescription">Role Description</label>
+          <textarea name="roleDescription" rows="8" cols="64" maxlength="512"></textarea>
+        </div>
+        <input class="ui primary button" type="submit" name="addRole" value="Create Role">
+      </form>
 
-    <br/>
+      <br/>
 
-    <table>
-      <thead>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Description</th>
-        <th></th>
-      </thead>
-      <tbody>
-        <?php
-          foreach ($db->baseQuery("SELECT * FROM ROLES") ?? array() as $role) {
-            create_row($role["RoleID"], $role["Name"], $role["Description"]);
-          }
+      <table class="ui celled table">
+        <thead>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Description</th>
+          <th></th>
+        </thead>
+        <tbody>
+          <?php
+            foreach ($db->baseQuery("SELECT * FROM ROLES") ?? array() as $role) {
+              create_row($role["RoleID"], $role["Name"], $role["Description"]);
+            }
 
-          function create_row($id, $name, $description){
-            $button =<<<EOT
-            <form method="POST" action="">
-              <input type="hidden" name="rm_role" value="$id">
-              <input type="submit" value="Delete Role">
-            </form>
+            function create_row($id, $name, $description){
+              $button =<<<EOT
+              <form method="POST" action="">
+                <input type="hidden" name="rm_role" value="$id">
+                <input class="ui button" type="submit" value="Delete Role">
+              </form>
 EOT;
 
-            $message = <<<EOT
-            <tr>
-              <td>$id</td>
-              <td>$name</td>
-              <td>$description</td>
-              <td>$button</td>
-            </tr>
+              $message = <<<EOT
+              <tr>
+                <td>$id</td>
+                <td>$name</td>
+                <td>$description</td>
+                <td>$button</td>
+              </tr>
 EOT;
-          echo $message;
-          }
-        ?>
-      </tbody>
-    </table>
+            echo $message;
+            }
+          ?>
+        </tbody>
+      </table>
+    </main>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . "/theatre_planner/pages/footer.html"; ?>
   </body>
 </html>
--- pages/admin/users.php
+++ pages/admin/users.php
@@ -19,50 +19,66 @@
   <head>
     <meta charset="utf-8">
     <title>Theatre Planner | Users</title>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . "/theatre_planner/pages/head.html"; ?>
   </head>
   <body>
     <!-- TODO add sidebar / static nav -->
-    <form action="" method="post">
-      <input required="true" type="text" name="userName" maxlength="32">
-      <input required="true" type="email" name="userMail" maxlength="64">
-      <input type="submit" name="addUser" value="Create User">
-    </form>
+    <main class="ui text container">
+      <form class="ui form" action="" method="post">
+        <div class="three fields">
+          <div class="required field">
+            <label for="userName">Name</label>
+            <input required="true" type="text" name="userName" maxlength="32">
+          </div>
+          <div class="required field">
+            <label for="userMail">E-Mail</label>
+            <input required="true" type="email" name="userMail" maxlength="64">
+          </div>
+          <div class="field">
+            <label>&nbsp;</label>
+            <input class="ui primary button" type="submit" name="addUser" value="Create User">
+          </div>
 
-    <br/>
+        </div>
+      </form>
 
-    <table>
-      <thead>
-        <th>ID</th>
-        <th>Name</th>
-        <th>E-Mail</th>
-        <th></th>
-      </thead>
-      <tbody>
-        <?php
-          foreach ($db->baseQuery("SELECT UserID, Name, Mail, Admin FROM USERS WHERE Admin = 0") ?? array() as $user) {
-            create_row($user["UserID"], $user["Name"], $user["Mail"]);
-          }
+      <br/>
 
-          function create_row($id, $name, $mail){
-            $button =<<<EOT
-            <form method="POST" action="">
-              <input type="hidden" name="rm_user" value="$id">
-              <input type="submit" value="Delete User">
-            </form>
+      <table class="ui celled table">
+        <thead>
+          <th>ID</th>
+          <th>Name</th>
+          <th>E-Mail</th>
+          <th></th>
+        </thead>
+        <tbody>
+          <?php
+            foreach ($db->baseQuery("SELECT UserID, Name, Mail, Admin FROM USERS WHERE Admin = 0") ?? array() as $user) {
+              create_row($user["UserID"], $user["Name"], $user["Mail"]);
+            }
+
+            function create_row($id, $name, $mail){
+              $button =<<<EOT
+              <form method="POST" action="">
+                <input type="hidden" name="rm_user" value="$id">
+                <input class="ui button" type="submit" value="Delete User">
+              </form>
 EOT;
 
-            $message = <<<EOT
-            <tr>
-              <td>$id</td>
-              <td>$name</td>
-              <td><a href="mailto:$mail">$mail</a></td>
-              <td>$button</td>
-            </tr>
+              $message = <<<EOT
+              <tr>
+                <td>$id</td>
+                <td>$name</td>
+                <td><a href="mailto:$mail">$mail</a></td>
+                <td>$button</td>
+              </tr>
 EOT;
-          echo $message;
-          }
-        ?>
-      </tbody>
-    </table>
+            echo $message;
+            }
+          ?>
+        </tbody>
+      </table>
+    </main>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . "/theatre_planner/pages/footer.html"; ?>
   </body>
 </html>
