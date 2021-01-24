23ca54be563d318776debeb3cb15a912d5cd5c65
Malte <malte.liam@outlook.com>
2021-01-24 01:54:17 +0100

    Add user pages

---
+++ pages/dashboard.php
@@ -0,0 +1,25 @@
+<?php
+require_once $_SERVER["DOCUMENT_ROOT"] . "/theatre_planner/php/auth/sessionValidate.php";
+if(!$loggedIn){
+  header("location:/theatre_planner/index.php");
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Theatre Planner | Dashboard</title>
+    <?php include $_SERVER['DOCUMENT_ROOT'] . "/theatre_planner/pages/head.html"; ?>
+  </head>
+  <body>
+    <?php include "nav.php" ?>
+    <main class="ui container">
+      User Dashboard
+    </main>
+    <?php include $_SERVER["DOCUMENT_ROOT"] . "/theatre_planner/pages/footer.html" ?>
+    <script type="text/javascript">
+      document.getElementById("nav_dashboard").className="active item";
+    </script>
+  </body>
+</html>
---
+++ pages/nav.php
@@ -0,0 +1,19 @@
+<?php session_start(); ?>
+<nav class="ui secondary pointing stackable menu">
+  <div class="ui container">
+    <div class="header item">Theatre Planner</div>
+    <a href="./dashboard.php" class="item" id="nav_dashboard">Dashboard</a>
+    <a href="./practices.php" class="item" id="nav_practices">Practices</a>
+    <a href="./personal.php" class="item" id="nav_personal">Personal Data</a>
+    <div class="right menu">
+      <?php if($_SESSION["Admin"]){
+        echo '<div class="ui item"><a href="/theatre_planner/pages/admin/dashboard.php" class="mini ui icon button" title="Change to Admin Dashboard"><i class="chess queen icon"></i></a></div>';
+      } ?>
+      <div class="ui item">
+        <form action="/theatre_planner/php/auth/logout.php" method="post">
+          <button type="submit" name="logout" class="mini ui right labeled icon button"><i class="sign-out icon"></i><?php echo $_SESSION["UserName"]; ?></button>
+        </form>
+      </div>
+    </div>
+  </div>
+</nav>
