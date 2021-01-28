eecb6e60ebdf94f13c3cd68305d2bd48f2c03dea
Malte <malte.liam@outlook.com>
2021-01-28 22:46:18 +0100

    Clean um Scene management. Closes #11

--- pages/admin/scenes.php
+++ pages/admin/scenes.php
@@ -13,20 +13,27 @@
   $inserted = true;
 
  if(isset($_POST["addScene"])){
+   // Add a new scene
    $db->update("INSERT INTO SCENES VALUES (NULL, ?, ?)", "ss", array($_POST["sceneName"], $_POST["sceneDescription"]));
  } elseif (isset($_POST["rm_scene"])){
+   // Delete a scene
    if(!$db->update("DELETE FROM SCENES WHERE SceneID=?", "i", array($_POST["rm_scene"]))){
+     // If delete fails due to foreign key references, delete dependencies first
      $dependencies = $db->prepareQuery("SELECT FeatureID FROM FEATURES WHERE SceneID=?", "i", array($_POST["rm_scene"]));
      foreach ($dependencies as $dependency) {
        $db->update("DELETE FROM FEATURES WHERE FEATUREID=?","i", array($dependency["FeatureID"]));
      }
+     // Delete scene again
      $db->update("DELETE FROM SCENES WHERE SceneID=?", "i", array($_POST["rm_scene"]));
    }
  } elseif (isset($_POST["rm_features"])) {
+   // Remove a role-scene relation
    $db->update("DELETE FROM FEATURES WHERE FeatureID=?", "i", array($_POST["rm_features"]));
  } elseif (isset($_POST["addFeature"])) {
+   // add a role-scene relation
    $db->update("INSERT INTO FEATURES VALUES (NULL, ?, ?, ?)", "iii", array($_POST["SceneID"], $_POST["newFeature"], (isset($_POST["isMandatory"])?1:0)));
  } elseif(isset($_POST["toggle_mandatory"])){
+   //Toggle whether the role is mandatory for the scene or not
    $isMandatory = $db->prepareQuery("SELECT Mandatory FROM FEATURES WHERE FeatureID=?","i", array($_POST["toggle_mandatory"]))[0]["Mandatory"];
    $db->update("UPDATE FEATURES SET Mandatory=? WHERE FeatureID=?", "ii", array(!$isMandatory,$_POST["toggle_mandatory"]));
  }
@@ -58,131 +65,34 @@
 
       <div class="ui two stackable cards">
         <?php
+        require dirname(dirname(__DIR__))."/php/ui/admin/createSceneCard.php";
           $currentScene = array("SceneID"=>-1);
           $FreeRoles = array();
           $scenes = $db->baseQuery("SELECT SCENES.*, FEATURES.FeatureID, FEATURES.Mandatory, ROLES.RoleID, ROLES.Name AS Role FROM SCENES LEFT JOIN FEATURES ON SCENES.SceneID = FEATURES.SceneID LEFT JOIN ROLES ON FEATURES.RoleID = ROLES.RoleID ORDER BY SCENES.SceneID");
           if(!empty($scenes)){
             foreach ($scenes as $scene) {
+              // DUe to joins, a scene can (and probably will) appear multiple times with multiple roles; This is stored here until it is ready to display
               if($currentScene["SceneID"] != $scene["SceneID"]){
                 if($currentScene["SceneID"] != -1){
+                  // Print card before overwriting with new scene
                   $FreeRoles = $db->prepareQuery("SELECT RoleID, Name FROM ROLES WHERE RoleID NOT IN (SELECT ROLES.RoleID FROM ROLES, FEATURES WHERE ROLES.RoleID = FEATURES.RoleID AND FEATURES.SceneID = ?)", "i", array($currentScene["SceneID"]));
-                  createCard($currentScene["SceneID"], $currentScene["Name"], $currentScene["Description"], $currentScene["Role"], $currentScene["FeatureID"], $currentScene["Mandatory"], $FreeRoles);
+                  echo createSceneCard($currentScene["SceneID"], $currentScene["Name"], $currentScene["Description"], $currentScene["Role"], $currentScene["FeatureID"], $currentScene["Mandatory"], $FreeRoles);
                 }
+                // Overwrite with new values
                 $currentScene = $scene;
                 $currentScene["Role"] = array($currentScene["Role"]);
                 $currentScene["Mandatory"] = array($currentScene["Mandatory"]);
                 $currentScene["FeatureID"] = array($currentScene["FeatureID"]);
               } else {
+                // Add values to the existing scene
                 array_push($currentScene["Role"], $scene["Role"]);
                 array_push($currentScene["Mandatory"], $scene["Mandatory"]);
                 array_push($currentScene["FeatureID"], $scene["FeatureID"]);
               }
             }
+            // Print the last card since it didn't ge triggered
             $FreeRoles = $db->prepareQuery("SELECT RoleID, Name FROM ROLES WHERE RoleID NOT IN (SELECT ROLES.RoleID FROM ROLES, FEATURES WHERE ROLES.RoleID = FEATURES.RoleID AND FEATURES.SceneID = ?)", "i", array($currentScene["SceneID"]));
-            createCard($currentScene["SceneID"], $currentScene["Name"], $currentScene["Description"], $currentScene["Role"], $currentScene["FeatureID"], $currentScene["Mandatory"], $FreeRoles);
-
-          }
-
-          function createCard($SceneID, $Name, $Description, $Roles, $Features, $Mandatory, $FreeRoles){
-            global $lang;
-            $role_rows = "";
-            foreach ($Roles as $index => $role) {
-              if($role == ""){
-                continue;
-              }
-              $mandatoryColour = "";
-              $mandatory_appendix = $lang->mandatory_appendix;
-              if($Mandatory[$index]){
-                $mandatoryColour = "orange";
-                $mandatory_appendix = "";
-              }
-              $role_rows .= <<<EOT
-              <tr>
-                <td>$role</td>
-                <td>
-                  <form action="" method="post">
-                    <input type="hidden" name="toggle_mandatory" value ="$Features[$index]">
-                    <button title="{$lang->admin_prefix}$mandatory_appendix{$lang->mandatory}" type="submit" style="cursor:pointer" class="ui $mandatoryColour label">
-                      <i class="fitted exclamation icon"></i>
-                    </button>
-                  </form>
-                </td>
-                <td>
-                  <form action="" method="POST">
-                    <input type="hidden" value="$Features[$index]" name="rm_features">
-                      <button type="submit" class="ui red icon button"><i class="trash icon"></i></button>
-                    </form>
-                  </td>
-              </tr>
-EOT;
-            }
-
-            $dialog_options = "";
-            if(count($FreeRoles==null?array():$FreeRoles)>0){
-              foreach($FreeRoles as $freeRole){
-                $dialog_options .= '<div class="item" data-value ="' . $freeRole["RoleID"] . '">' . $freeRole["Name"] . '</div>';
-              }
-            }
-
-          $role_dialog =<<<EOT
-          <tr>
-            <form action="" method="post">
-              <td>
-                <div class="field">
-                  <div class="ui selection dropdown">
-                    <input type="hidden" name="newFeature">
-                    <i class="dropdown icon"></i>
-                    <div class="default text">{$lang->features}</div>
-                      <div class="menu">
-                        $dialog_options
-                      </div>
-                  </div>
-                </div>
-              </td>
-              <td>
-              <div class="ui toggle checkbox">
-                <input type="checkbox" name="isMandatory" checked="true">
-              </div>
-              </td>
-              <td>
-                <input type="hidden" name="SceneID" value="$SceneID">
-                <button class="ui primary icon button" type="submit" name="addFeature"><i class="plus icon"></i></button>
-              </td>
-            </form>
-          </tr>
-EOT;
-
-          $button =<<<EOT
-          <form method="POST" action="" style="margin-bottom:0;">
-            <input type="hidden" name="rm_scene" value="$SceneID">
-            <button class="ui bottom attached red button" style="width:100%" type="submit"><i class="trash icon"></i></button>
-          </form>
-EOT;
-
-          $card =<<<EOT
-          <div class="ui card">
-            <div class="content">
-              <div class="header">
-                $Name
-                <div class="right floated meta">#$SceneID</div>
-              </div>
-            </div>
-            <div class="content">
-              <div class="ui sub header">{$lang->description}</div>
-              $Description
-            </div>
-            <div class="content">
-              <div class="ui sub header">{$lang->roles}</div>
-              <table class="ui very basic table">
-                $role_rows
-                $role_dialog
-              </table>
-            </div>
-            $button
-          </div>
-EOT;
-          echo $card;
-
+            echo createSceneCard($currentScene["SceneID"], $currentScene["Name"], $currentScene["Description"], $currentScene["Role"], $currentScene["FeatureID"], $currentScene["Mandatory"], $FreeRoles);
           }
         ?>
       </div>
---
+++ php/ui/admin/createSceneCard.php
@@ -0,0 +1,144 @@
+<?php
+/**
+* Creates a card that displays information about a scene and a form to add, remove and priorize roles.
+*
+* @param int|string $SceneID ID of the Scene
+* @param string $Name name of the Scene
+* @param string $Description Description of the Scene
+* @param array $Roles Array of names of  featured roles
+* @param array $Features Array of Primary keys of role-scene relationships in the same orrder as roles
+* @param array $Mandatory Array of booleans wheter the role is mandatory for the scene or not - in the same order as roles
+* @param array $FreeRoles Array of Role ID and Name of Roles that have not yet been assigned to the scene
+*
+* @return string Card template
+*/
+function createSceneCard($SceneID, $Name, $Description, $Roles, $Features, $Mandatory, $FreeRoles){
+  global $lang;
+
+  $role_rows = createRoleRows($Roles, $Mandatory, $Features);
+
+  $role_dialogue = createRoleDialogue($FreeRoles, $SceneID);
+
+$button =<<<EOT
+<form method="POST" action="" style="margin-bottom:0;">
+  <input type="hidden" name="rm_scene" value="$SceneID">
+  <button class="ui bottom attached red button" style="width:100%" type="submit"><i class="trash icon"></i></button>
+</form>
+EOT;
+
+$card =<<<EOT
+<div class="ui card">
+  <div class="content">
+    <div class="header">
+      $Name
+      <div class="right floated meta">#$SceneID</div>
+    </div>
+  </div>
+  <div class="content">
+    <div class="ui sub header">{$lang->description}</div>
+    $Description
+  </div>
+  <div class="content">
+    <div class="ui sub header">{$lang->roles}</div>
+    <table class="ui very basic table">
+      $role_rows
+      $role_dialogue
+    </table>
+  </div>
+  $button
+</div>
+EOT;
+return $card;
+}
+
+/**
+* Creates a table row with name, mandatory-toggle-form and delete-relationship button for each role.
+*
+* @param array $Roles Array of names of  featured roles
+* @param array $Features Array of Primary keys of role-scene relationships in the same orrder as roles
+* @param array $Mandatory Array of booleans wheter the role is mandatory for the scene or not - in the same order as roles
+*
+* @return string Template string for table rows
+*/
+function createRoleRows($Roles, $Mandatory, $Features){
+  global $lang;
+
+  $role_rows = "";
+  foreach ($Roles as $index => $role) {
+    if($role == ""){
+      continue;
+    }
+    $mandatoryColour = "";
+    $mandatory_appendix = $lang->mandatory_appendix;
+    if($Mandatory[$index]){
+      $mandatoryColour = "orange";
+      $mandatory_appendix = "";
+    }
+    $role_rows .= <<<EOT
+    <tr>
+      <td>$role</td>
+      <td>
+        <form action="" method="post">
+          <input type="hidden" name="toggle_mandatory" value ="$Features[$index]">
+          <button title="{$lang->admin_prefix}$mandatory_appendix{$lang->mandatory}" type="submit" style="cursor:pointer" class="ui $mandatoryColour label">
+            <i class="fitted exclamation icon"></i>
+          </button>
+        </form>
+      </td>
+      <td>
+        <form action="" method="POST">
+          <input type="hidden" value="$Features[$index]" name="rm_features">
+            <button type="submit" class="ui red icon button"><i class="trash icon"></i></button>
+          </form>
+        </td>
+    </tr>
+EOT;
+  }
+  return $role_rows;
+}
+
+/**
+* Creates a Dropdown Dialogue to add roles to the scene.
+*
+* @param array $FreeRoles Array of Role ID and Name of Roles that have not yet been assigned to the scene
+* @param int|string $SceneID ID of the Scene
+*/
+function createRoleDialogue($FreeRoles, $SceneID){
+  global $lang;
+  $dialogue_options = "";
+  if(count($FreeRoles??array())>0){
+    foreach($FreeRoles as $freeRole){
+      $dialog_options .= '<div class="item" data-value ="' . $freeRole["RoleID"] . '">' . $freeRole["Name"] . '</div>';
+    }
+  }
+
+$role_dialogue =<<<EOT
+<tr>
+  <form action="" method="post">
+    <td>
+      <div class="field">
+        <div class="ui selection dropdown">
+          <input type="hidden" name="newFeature">
+          <i class="dropdown icon"></i>
+          <div class="default text">{$lang->features}</div>
+            <div class="menu">
+              $dialogue_options
+            </div>
+        </div>
+      </div>
+    </td>
+    <td>
+    <div class="ui toggle checkbox">
+      <input type="checkbox" name="isMandatory" checked="true">
+    </div>
+    </td>
+    <td>
+      <input type="hidden" name="SceneID" value="$SceneID">
+      <button class="ui primary icon button" type="submit" name="addFeature"><i class="plus icon"></i></button>
+    </td>
+  </form>
+</tr>
+EOT;
+  return $role_dialogue;
+}
+?>
