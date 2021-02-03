1f334b8904e517751af4e24e529001e12b8bb768
Malte <malte.liam@outlook.com>
2021-02-03 08:37:32 +0100

    Add Documentation

--- php/ui/admin/createCompactTable.php
+++ php/ui/admin/createCompactTable.php
@@ -1,23 +1,43 @@
 <?php
 require dirname(dirname(__DIR__))."/utils/compact.php";
+
+/**
+* Creates the body of the overview table
+*
+* @param array $everything query results of all scenes containing ScenID, Scene name, scene sequence and all associated roles and actors with corresponding relation IDs
+*
+* @return string template of the body
+*/
 function createCompactTable($everything){
   $body = "";
   $sceneObject = new compactScene(NULL);
   foreach ($everything??array() as $scene) {
+    // Aggregation of coherent data for each scene
     if($sceneObject->id != $scene["SceneID"]){
+      // Aggregation complete, generating table row(s) and starting new aggregation
       if(isset($sceneObject->id)){
         $body .= generateSceneRow($sceneObject);
       }
       $sceneObject = new compactScene($scene);
     } else {
+      // Aggregating data
       $sceneObject->addRelations($scene);
     }
   }
+  // Triggering last scene
   $body .= generateSceneRow($sceneObject);
+  // Generating "add scene" dialogue
   $body .= generateAddSceneRow($sceneObject->sequence+1);
   return $body;
 }
 
+/**
+* Generate tablerow(s) for a scene
+*
+* @param array $scene the compactScene object of the scene to print
+*
+* @return string template for scene row(s)
+*/
 function generateSceneRow($scene){
   $rowspan = $scene->getRelationCount()+1;
   $sceneCell = $sceneCell = <<<EOT
@@ -26,26 +46,32 @@ EOT;
   $roleActorCells = "";
   $sceneRow = "";
   if(!empty($scene->parties[0]["FeatureID"])){
+    // If there is at least one role associated, print it in the next cell
     $actorDropdown = "";
     if(empty($scene->parties[0]["PlaysID"])){
+      // If there is an actor associated with the role, print corresponding pre-selected form in the next cell...
       $actorDropdown = generateAddActorDropdown($scene->parties[0]["RoleID"]);
     } else {
+      // ... or generate form to assign an exissting actor
       $actorDropdown = generateChangeActorDropdown($scene->parties[0]);
     }
 
     $roleActorCells = '<td>'.generateChangeRoleDropdown($scene->parties[0]).'</td><td>'.$actorDropdown.'</td>';
     $sceneRow = "<tr>".$sceneCell.$roleActorCells."</tr>";
     if($scene->getRelationCount()>1){
+      // If there are more than one roles associated, print coresponding roles
       for ($i=1; $i < $scene->getRelationCount(); $i++) {
         if(empty($scene->parties[$i]["PlaysID"])){
+          // If there is an actor associated with the role, print corresponding pre-selected form in the next cell...
           $actorDropdown = generateAddActorDropdown();
         } else {
+          // ... or generate form to assign an exissting actor
           $actorDropdown = generateChangeActorDropdown($scene->parties[$i]);
         }
         $sceneRow .= "<tr><td>".generateChangeRoleDropdown($scene->parties[$i])."</td><td>".$actorDropdown."</td></tr>";
       }
     }
-    $sceneRow .= '<tr><td>'.generateAddRoleDropdown($scene->id).'</td><td></td><tr>';
+    // Printing form to add role to scene, indifferent of whether there already are other actors
     $sceneRow .= '<tr><td>'.generateAddRoleDropdown($scene->id, $scene->getRoles()).'</td><td></td><tr>';
   } else {
     $sceneRow .= '<tr>'.$sceneCell.'<td>'.generateAddRoleDropdown($scene->id, $scene->getRoles()).'</td><td></td><tr>';
@@ -54,13 +80,20 @@ EOT;
   return $sceneRow;
 }
 
-
+/**
+* Generates a dropdown to change an associated actor with pre-selection of the current actor
+*
+* @param array $selected associative array of the selected array including name, id and relation-id
+*
+* @return string form template
+*/
 function generateChangeActorDropdown($selected){
   global $db;
   $options = $db->baseQuery("SELECT UserID AS ID, Name FROM USERS")??array();
 
   $selections = "";
   foreach ($options as $option) {
+    // Determine the selected option and generate options template
     $active = $option["ID"]==$selected["UserID"]?"active selected ":"";
     $selections .= '<div class="'.$active.'item" data-value="'.$option["ID"].'">'.$option["Name"].'</div>';
   }
@@ -86,6 +119,13 @@ EOT;
   return $form;
 }
 
+/**
+* Generates a dropdown to change an associated role with pre-selection of the current role
+*
+* @param array $selected associative array of the selected role including name, id and relation-id
+*
+* @return string form template
+*/
 function generateChangeRoleDropdown($selected){
   global $db;
   $options = $db->baseQuery("SELECT RoleID AS ID, Name FROM ROLES")??array();
@@ -93,6 +133,7 @@ function generateChangeRoleDropdown($selected){
 
   $selections = "";
   foreach ($options as $option) {
+    // Determine the selected option and generate options template
     $active = $option["ID"]==$selected["RoleID"]?"active selected ":"";
     $selections .= '<div class="'.$active.'item" data-value="'.$option["ID"].'">'.$option["Name"].'</div>';
   }
@@ -118,6 +159,13 @@ EOT;
   return $form;
 }
 
+/**
+* Generates a dropdown to assign an existing actor to a role
+*
+* @param int|string $RoleID ID of the role to assign to
+*
+* @return string form template
+*/
 function generateAddActorDropdown($RoleID){
   global $lang;
   global $db;
@@ -143,17 +191,27 @@ EOT;
   return $form;
 }
 
+/**
+* Generates a dropdown to assign an existing or a new role to a scene
+*
+* @param int|string $SceneID of the scene to assign to
+* @param array $excludeRoles array containing IDs of the roles that are already assigned to the scene
+*
+* @return string form template
+*/
 function generateAddRoleDropdown($SceneID, $excludeRoles){
   global $lang;
   global $db;
   $excludeIDs = array();
   foreach ($excludeRoles as $excludeRole) {
+    // Generating associative array for better performance later on
     $excludeIDs[$excludeRole["ID"]]=true;
   }
   $roles = $db->baseQuery("SELECT RoleID, Name FROM ROLES");
   $selections = "";
   foreach ($roles as $option) {
     if(empty($excludeIDs[$option["RoleID"]])){
+      // generating only options for roles that are not excluded
       $selections .= '<div class="item" data-value="'.$option["RoleID"].'">'.$option["Name"].'</div>';
     }
   }
@@ -175,6 +233,13 @@ EOT;
   return $form;
 }
 
+/**
+* Generates a table row with a form to add a new scene
+*
+* @param int|string $sequence Sequence of the new role
+*
+* @return string row template
+*/
 function generateAddSceneRow($sequence){
   global $lang;
   $form = <<<EOT
--- php/utils/compact.php
+++ php/utils/compact.php
@@ -1,4 +1,12 @@
 <?php
+/**
+* A wrapper class for scenes to display in the compact table.
+*
+* @property int|string $id ID of the Scene
+* @property string $title Title of the scene
+* @property int $sequence Sequence of the scene
+* @property array $parties All relations of the scene containing Name, ID and Relation-ID of role and actor (if present)
+*/
 class compactScene{
   public $id;
   public $title;
@@ -18,6 +26,11 @@ class compactScene{
     }
   }
 
+  /**
+  * Returns all associated actors with Name, ID and Relation-ID
+  *
+  * @return array all associated actors with Name, ID and Relation-ID
+  */
   public function getActors(){
     $actors = array();
     foreach ($this->parties as $actor) {
@@ -26,6 +39,11 @@ class compactScene{
     return $actors;
   }
 
+  /**
+  * Returns all associated roles with Name, ID and Relation-ID
+  *
+  * @return array all associated roles with Name, ID and Relation-ID
+  */
   public function getRoles(){
     $roles = array();
     foreach ($this->parties as $role) {
@@ -34,10 +52,22 @@ class compactScene{
     return $roles;
   }
 
+  /**
+  * Adds Roles and Actors form a given query result to the local array
+  *
+  * @param array $scene query reults with all required information stored in the local array
+  *
+  * @see $parties
+  */
   public function addRelations($scene){
     $this->parties[] = array("FeatureID"=>$scene["FeatureID"], "RoleID"=>$scene["RoleID"], "RoleName"=>$scene["Role"], "PlaysID" => $scene["PlaysID"], "UserID"=>$scene["UserID"], "UserName"=>$scene["Name"]);
   }
 
+  /**
+  * Returns the amount of associated roles
+  *
+  * @return int Amount of associated roles
+  */
   public function getRelationCount(){
     return count($this->parties??array());
   }
